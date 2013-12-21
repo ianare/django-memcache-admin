11af257963f6293d9434b7b12331fd3a04b024b2
ianaré sévi <ianare@gmail.com>
2013-12-21 17:48:54 +0100

    wrap admin views to avoid cache/perm problems

--- memcache_admin/admin.py
+++ memcache_admin/admin.py
@@ -3,8 +3,9 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 from .models import Memcached
-from .urls import urlpatterns
 
+from django.conf.urls import patterns, url
+from . import views
 
 class MemcachedAdmin(admin.ModelAdmin):
     def has_add_permission(self, request):
@@ -15,6 +16,12 @@ class MemcachedAdmin(admin.ModelAdmin):
 
     def get_urls(self):
         urls = super(MemcachedAdmin, self).get_urls()
+        urlpatterns = patterns('memcache_admin.views',
+            url(r'^$', self.admin_site.admin_view(views.dashboard), name='dashboard'),
+            url(r'^flush/$', self.admin_site.admin_view(views.flush), name='flush'),
+            url(r'^stats/(?P<server_name>.+)$', self.admin_site.admin_view(views.stats), name='stats'),
+            url(r'^slabs/(?P<server_name>.+)$', self.admin_site.admin_view(views.slabs), name='slabs'),
+        )
         return urlpatterns + urls
 
 
