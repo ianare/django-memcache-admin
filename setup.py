d1bab8ea786e65dfeaf4b4f7f2901b71de00116b
ianaré sévi <ianare@gmail.com>
2013-12-27 23:11:00 +0100

    ready for 0.0.3 release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-memcache-admin",
-    version='0.0.2',
+    version='0.0.3',
     author="Ianaré Sévi",
     author_email="ianare@gmail.com",
     url="https://github.com/ianare/django-memcache-admin",
